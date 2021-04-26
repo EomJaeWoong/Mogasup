7abd47f08f884d4505b4eb82110c2c4cc7582433
DESKTOP-KVCQHCD\multicampus <uaio77@gmail.com>
2021-04-26 16:56:57 +0900

    ADD : Add Backend Project

---
+++ mogasupServer/src/main/java/com/web/mogasup/MogasupServerApplication.java
@@ -0,0 +1,13 @@
+package com.web.mogasup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogasupServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MogasupServerApplication.class, args);
+	}
+
+}
---
+++ mogasupServer/src/test/java/com/web/mogasup/MogasupServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.mogasup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogasupServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
