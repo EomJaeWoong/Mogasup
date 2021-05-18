b37c2d5aa6366a1f4eb01d8fedfc6696c48ace84
username <sis6617@naver.com>
2021-05-18 10:17:15 +0900

    ADD : Unity Game Zone 추가

---
+++ unity/Assets/script/zone/GameList.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GameList : MonoBehaviour
+{
+    public RectTransform uiGroup;
+    PlayerMovement player;
+
+    public void Enter(PlayerMovement p)
+    {
+        player = p;
+
+        uiGroup.anchoredPosition = Vector3.zero;
+    }
+
+    public void Exit()
+    {
+        uiGroup.anchoredPosition = Vector3.down * 2000;
+    }
+
+    public void OpenGolfGameBtn()
+    {
+        SceneManager.LoadScene("GolfGameScene");
+    }
+
+    public void OpenSnakeGameBtn()
+    {
+        SceneManager.LoadScene("SnakeGameScene");
+    }
+
+}
