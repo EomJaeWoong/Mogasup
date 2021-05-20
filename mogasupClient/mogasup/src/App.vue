b751a14c693c05abc7eb94dcdc1955f76d27036f
username <sis6617@naver.com>
2021-05-20 21:39:47 +0900

    INIT : 프론트엔드 다운로드 페이지 추가

---
+++ mogasupClient/mogasup/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <h1>MoGaSup</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+      <Footer />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Footer from "./components/Footer.vue";
+export default {
+  components: { Footer },
+  name: "App",
+
+  data: () => ({
+    //
+  })
+};
+</script>
